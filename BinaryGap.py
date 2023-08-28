fade55a5adc54b4b55aaec534ef3f32891589ba3
Camila Souza <camila.odsouza@gmail.com>
2023-08-28 15:43:02 +0200

    Update BinaryGap.py
    Optimized
    
    (
    - I don't need to find the first 1, since I can assume the first number will be 1 after transforming from int to bin
    - I don't need to transform to list. I can just iterate through the binary number
    )

--- BinaryGap.py
+++ BinaryGap.py
@@ -2,19 +2,13 @@
 # print("this is a debug message")
 
 def solution(N):
-    listbin = [int(d) for d in str(bin(N))[2:]]
+    N = bin(N)[2:]
     count = 0
-    gap = 0
-    for i in range(len(listbin)-1):
-        if listbin[i] == 1:
-            if listbin[i+1] == 0:
-                count = 0
-                for j in range(i+1, len(listbin)):
-                    if listbin[j] == 0:
-                        count += 1
-                    if listbin[j] == 1:
-                        if count > gap:
-                            gap = count
-                        break
-    
-    return gap
+    maxgap = 0
+    for k in N:
+        if int(k) == 0:
+            count += 1
+        elif int(k) == 1:
+            maxgap = max(count, maxgap)
+            count = 0
+    return maxgap
